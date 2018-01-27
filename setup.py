1b1eb2e39e609a7564eb36df7f29b9d5555def96
chris48s <chris.shaw480@gmail.com>
2018-01-27 19:03:24 +0000

    package module

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+def _get_description():
+    try:
+        path = os.path.join(os.path.dirname(__file__), 'README.rst')
+        with open(path, encoding='utf-8') as f:
+            return f.read()
+    except IOError:
+        return ''
+
+setup(
+    name='uk_election_ids',
+    author="chris48s",
+    license="MIT",
+    url="https://github.com/DemocracyClub/uk-election-ids/",
+    packages=['uk_election_ids'],
+    description='Create Democracy Club Election Identifiers',
+    long_description=_get_description(),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
