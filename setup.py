75dd38bb6eac23b25b8b917be5a7eaca85c7c903
Sym Roe <sym.roe@talusdesign.co.uk>
2023-01-24 19:55:49 +0000

    Include data in package

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
     license="MIT",
     url="https://github.com/DemocracyClub/uk-election-ids/",
     packages=["uk_election_ids"],
+    package_data={'uk_election_ids':  ['data/*.json']},
     description="Create Democracy Club Election Identifiers",
     long_description=_get_description(),
     long_description_content_type="text/markdown",
