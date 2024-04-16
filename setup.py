6e225893b2e08eea173311c66f419a8b5f3d64db
Sym Roe <sym.roe@talusdesign.co.uk>
2024-04-16 07:39:28 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def _get_description():
 
 setup(
     name="uk_election_ids",
-    version="0.7.5",
+    version="0.8.0",
     author="chris48s",
     license="MIT",
     url="https://github.com/DemocracyClub/uk-election-ids/",
