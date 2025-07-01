98d33c44f68a67de57aa65bc000dc163c6ab54dd
chris48s <git@chris-shaw.dev>
2025-07-01 13:39:53 +0100

    move id regex tests

--- tests/test_id_requirements.py
+++ tests/test_id_requirements.py
@@ -26,6 +26,25 @@ class TestIDRequirementsJson(TestCase):
 
 
 class TestIDRequirementsMatcher(TestCase):
+    def test_meta_data_matcher_regex(self):
+        matcher = IDRequirementsMatcher("parl.corby.2022-05-04", nation="ENG")
+        assert matcher.match_id()[0] == "parl"
+
+        matcher = IDRequirementsMatcher(
+            "parl.stroud.by.2022-05-04", nation="ENG"
+        )
+        assert matcher.match_id()[0] == "parl.*.by"
+
+        matcher = IDRequirementsMatcher(
+            "parl.reading.tilehurst.by.2022-05-04", nation="ENG"
+        )
+        assert matcher.match_id()[0] == "parl.*.by"
+
+        matcher = IDRequirementsMatcher(
+            "nonsense.parl.stroud.by.2022-05-04", nation="ENG"
+        )
+        assert matcher.match_id()[0] is None
+
     def test_matcher_england(self):
         # Local election pre 2022 legislation
         result = IDRequirementsMatcher("local.stroud.2022-05-04", nation="ENG")
